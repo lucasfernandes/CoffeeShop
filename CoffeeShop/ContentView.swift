f0e3aa5ba925dca9cbc753171c6679b1df75c9c9
Lucas Silveira <lucas.fernandes.silveira@gmail.com>
2019-09-21 15:39:45 -0300

    Initial Commit

---
+++ CoffeeShop/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CoffeeShop
+//
+//  Created by Lucas Silveira on 21/09/19.
+//  Copyright © 2019 Lucas Silveira. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
